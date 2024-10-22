16892d93c50e74791c21bcd9318d673bf511a888
Sava Mikalacki <mikalackis@gmail.com>
2024-10-22 17:27:29 +0200

    Make optional libraries conditional

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ LOCAL_SRC_FILES := NewPipe.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
